8329214e8585394c2f9f263ae3bf6a1057059e48
AdairBlack <eternaledgar@qq.com>
2023-04-27 00:02:57 +0800

    Update 135.cpp

--- 135.cpp
+++ 135.cpp
@@ -3,31 +3,25 @@ class Solution
 public:
     int candy(vector<int> &ratings)
     {
-        int count = ratings.size();
-
-        if (count <= 1)
-        {
-            return count;
-        }
-
-        vector<int> candies(count, 1);
-
+        int result = ratings.size();
+        vector<int> candies(result, 1);
         for (int i = 1; i < ratings.size(); i++)
         {
-            if (ratings[i] > ratings[i - 1])
+            if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1])
             {
+                result += candies[i - 1] + 1 - candies[i];
                 candies[i] = candies[i - 1] + 1;
             }
         }
-
         for (int i = ratings.size() - 2; i >= 0; i--)
         {
             if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
             {
+                result += candies[i + 1] + 1 - candies[i];
                 candies[i] = candies[i + 1] + 1;
             }
         }
 
-        return accumulate(candies.begin(), candies.end(), 0);
+        return result;
     }
 };
